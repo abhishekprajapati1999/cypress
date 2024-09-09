0e90cf324313924071aa27c5041bd4aaef17f808
abhishekprajapati1999 <abhishek1999.29@gmail.com>
2024-09-09 16:00:41 +0530

    Modified Config for envs

--- cypress.config.js
+++ cypress.config.js
@@ -12,8 +12,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-      // const version = config.env.version || "local";
-      // config.env = require(`./cypress/config/${version}.json`);
       return config;
     },
   },
